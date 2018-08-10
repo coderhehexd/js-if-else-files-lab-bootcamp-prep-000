7ca5d495c31fa534bf09326186c6e35aeedbbae9
Chris Li <cli78822@gmail.com>
2018-08-10 01:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var name = "Joe"
+var name = "Susan"
 var height = "70"
 
 if(name === "Susan"){
   console.log("The name is correct");
 }else{
   console.log("Expected the name to be Susan");
+}
+
+if(height === 70){
+  console.log("The height is correct")
 }
 
 // Don't worry about this
